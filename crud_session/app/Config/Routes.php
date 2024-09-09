0b354e4e308bc6dde393d9a696297c26fc5e9889
Edgar barbosa da silva junior <edgarbsj.dev@gmail.com>
2024-09-09 11:24:55 -0300

    adicionar primeiro Controller e Views iniciais

--- crud_session/app/Config/Routes.php
+++ crud_session/app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('pages/mostrar', 'Pages::mostrar');
+$routes->get('pages/mostrar/(:segment)', 'Pages::mostrar/$1');
